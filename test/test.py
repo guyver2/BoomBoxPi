47434f2b4e2bf15ac003a29b2522320cc2b3491c
Antoine Letouzey <antoine.letouzey@gmail.com>
2019-12-04 09:49:27 +0000

    Update test.py with a failing test

--- test/test.py
+++ test/test.py
@@ -2,8 +2,11 @@ import unittest
 
 class TestStringMethodsDummy(unittest.TestCase):
 
-    def test_dummy_1(self):
+    def test_dummy_pass(self):
         self.assertEqual(1,1)
+    
+    def test_dummy_fail(self):
+        self.assertEqual(1,0)
 
 if __name__ == '__main__':
     unittest.main()
