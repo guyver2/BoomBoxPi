a3da77eb274509186a222549e5984bcedbd22057
Antoine Letouzey <antoine.letouzey@gmail.com>
2020-02-22 12:22:02 +0000

    Resolve "Improve saving/loading of tracks database"

--- nfc.py
+++ nfc.py
@@ -6,7 +6,9 @@ import threading
 
 class NFC:
     READ = 0
-    WRITE = 1
+    WRITE_TRACK = 1
+    WRITE_PLAYLIST = 2
+
 
     def __init__(self, player):
         self.reader = SimpleMFRC522()
@@ -25,24 +27,29 @@ class NFC:
                         splits = text.split()
                         if len(splits) >= 2:
                             hashRequest = splits[1]
-                            if self.player.currentTrack is not None and hashRequest == self.player.currentTrack.hash:
+                            if splits[0] == "t" and self.player.currentTrack is not None and hashRequest == str(self.player.currentTrack.hash):
+                                print("requested track already playing", text)
                                 continue
-                            if self.player.currentPlaylist is not None and hashRequest == self.player.currentPlaylist.hash:
+                            if splits[0] == "p" and self.player.currentPlaylist is not None and hashRequest == str(self.player.currentPlaylist.hash):
+                                print("requested playlist already playing", text)
                                 continue
                             self.player.request(text)
-                if self.mode == NFC.WRITE:
-                    id, text = self.reader.write_no_block("t " + self.player.currentTrack.hash + " " + self.player.currentTrack.title)
+                if self.mode == NFC.WRITE_TRACK:
+                    id, text = self.reader.write_no_block("t " + str(self.player.currentTrack.hash))
+                    if id is not None:
+                        print("nfc written")
+                        self.mode = NFC.READ
+                if self.mode == NFC.WRITE_PLAYLIST:
+                    id, text = self.reader.write_no_block("p " + str(self.player.currentPlaylist.hash))
                     if id is not None:
                         print("nfc written")
                         self.mode = NFC.READ
         finally:
             GPIO.cleanup()
     
-    def switchMode(self):
-        if self.mode == NFC.READ:
-            self.mode = NFC.WRITE
-        else:
-            self.mode = NFC.READ
+    def switchMode(self, mode):
+        if mode in [NFC.READ, NFC.WRITE_TRACK, NFC.WRITE_PLAYLIST]:
+            self.mode = mode
 
 
 if __name__ == "__main__":
