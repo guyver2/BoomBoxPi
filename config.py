92196434a12e5ded132841096cc6d08410d1f154
Antoine Letouzey <antoine.letouzey@gmail.com>
2020-05-18 21:17:49 +0100

    12 wip, initial commit

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ class Config:
     PLAYLISTS_IMG_FOLDER = DATA + "covers/playlists/"
     DEFAULT_TRACK_IMG = TRACKS_IMG_FOLDER + "default.jpg"
     DEFAULT_PL_IMG = PLAYLISTS_IMG_FOLDER + "default.jpg"
+    WEBRADIOS_IMG_FOLDER = DATA + "covers/webradios/"
+    DEFAULT_WR_IMG = WEBRADIOS_IMG_FOLDER + "default.png"
     SPOTIPY_CLIENT_ID = "XXXXXXXX"
     SPOTIPY_CLIENT_SECRET = "XXXXXXXX"
---
+++ webRadioPlayer.py
@@ -0,0 +1,70 @@
+import threading
+import re
+import subprocess
+import alsaaudio
+
+class WebRadioPlayer(object):
+    def __init__(self):
+        self.stream_url = None
+        self.mpg123_process = None
+        self.mixer = alsaaudio.Mixer('PCM')
+        self.volume = self.get_volume()
+        self.muted = False
+
+    def mute(self):
+        self.muted = True
+        self.setVolume(self.volume)
+
+    def unmute(self):
+        self.muted = False
+        self.setVolume(self.volume)
+
+    def get_volume(self):
+        volume_left, volume_right = self.mixer.getvolume()
+        return (volume_left + volume_right)//2
+            
+    def set_volume(self, volume):
+        self.volume = volume
+        volume = 0 if self.muted else self.volume
+        self.mixer.setvolume(volume, 0)
+        self.mixer.setvolume(volume, 1)
+    
+    def play(self, stream_url):
+        self.stream_url = stream_url
+        cmd = ["mpg123", stream_url]
+        self.mpg123_process = subprocess.Popen(cmd)
+    
+    def stop(self):
+        if self.mpg123_process:
+            self.mpg123_process.kill()
+            self.mpg123_process = None
+        
+    def hard_stop(self):
+        proc = subprocess.Popen(['killall', 'mpg123'])
+
+
+
+if __name__ == "__main__":
+    import time
+    import sys
+
+    stream_url = "http://stream.srg-ssr.ch/m/couleur3/mp3_128"
+    if len(sys.argv) != 2:
+        print("using default station: couleur 3")
+    else:
+        stream_url = sys.argv[1]
+
+    wrp = WebRadioPlayer()
+    wrp.set_volume(10)
+    wrp.play(stream_url)
+    time.sleep(5)
+    print("changing volume")
+    wrp.set_volume(20)
+    time.sleep(5)
+    wrp.set_volume(30)
+    time.sleep(5)
+    wrp.set_volume(40)
+    time.sleep(5)
+    wrp.set_volume(50)
+    input("press enter to quit")
+    wrp.stop()
