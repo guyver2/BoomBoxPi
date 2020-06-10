b16c3873b76f3d82fc3f8f6b92b967f29da829cd
Antoine Letouzey <antoine.letouzey@gmail.com>
2020-06-10 16:37:38 +0100

    32: set max volume to 100 instead of 50

--- pot.py
+++ pot.py
@@ -8,11 +8,11 @@ I2C_SLAVE_COMMAND = 0x0703
 # set device address
 FileHandle = io.open("/dev/i2c-1", "rb", buffering=0)
 fcntl.ioctl(FileHandle, I2C_SLAVE_COMMAND, i2c_address)
-SENSIBILITY = 8
+SENSIBILITY = 1
+MAX_VALUE = 4095
 
 
 class Potentiometer:
-
     def __init__(self, player):
         self.lock = False
         self.player = player
@@ -29,10 +29,10 @@ class Potentiometer:
         global FileHandle
         while self.player.alive:
             values = list(FileHandle.read(2))
-            data = (values[0] * 256 + values[1]) / 4
+            data = int(100 * (values[0] * 256 + values[1]) / MAX_VALUE)
             if self.value == None or abs(self.value - data) > SENSIBILITY:
                 if not self.lock:
-                    self.player.setVolume(data / 20)
+                    self.player.setVolume(data)
                 self.value = data
             time.sleep(0.05)
 
@@ -40,6 +40,6 @@ class Potentiometer:
 if __name__ == "__main__":
     while True:
         value = list(FileHandle.read(2))
-        data = (value[0] * 256 + value[1]) / 4
+        data = (value[0] * 256 + value[1])
         print(data)
         time.sleep(0.1)
